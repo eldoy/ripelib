88b6e3f5848a6bb93d806b5285c0bda3947662c5
acmb <48370302+acmb@users.noreply.github.com>
2024-07-03 20:22:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-var ipv4 = require('./data/ipv4.json')
-var ipv6 = require('./data/ipv6.json')
 var ipaddr = require('ipaddr.js')
+
+function chunkify(array, size) {
+  var chunks = []
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size))
+  }
+  return chunks
+}
 
 module.exports = {
   ip2location: function (input) {
     var ip = ipaddr.parse(input)
     var data = []
+    var delimiter
 
     if (input.includes('.')) {
-      data = ipv4
+      delimiter = '.'
+      data = require('./data/ipv4.json')
     }
 
     if (input.includes(':')) {
-      data = ipv6
+      delimiter = ':'
+      data = require('./data/ipv6.json')
     }
 
-    var result = data.find(({ cidr }, idx) => {
-      return ip.match(ipaddr.parseCIDR(cidr))
-    })
+    var octet = input.split(delimiter)[0]
+
+    var result = chunkify(data, 50)
+      .filter((entry) => {
+        entry = [...new Set(entry.map(({ from }) => from.split(delimiter)[0]))]
+        return entry.includes(octet)
+      })
+      .flatMap((entry) => entry)
+      .find(({ cidr }) => {
+        return ip.match(ipaddr.parseCIDR(cidr))
+      })
 
     return { ip: input, country: result?.country }
   }
