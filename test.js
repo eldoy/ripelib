b1184e87ca1aa94edbbc781a5793c0ab06dc33a4
acmb <48370302+acmb@users.noreply.github.com>
2024-07-03 18:48:03 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var ripe = require('./index.js')
 
-var ip = '89.115.187.83'
-//var ip = '2001:818:dcd4:c100:3139:b31a:7fe8:3eba'
+// var ip = '89.115.187.83'
+// var ip = '2001:818:dcd4:c100:3139:b31a:7fe8:3eba'
+var ip = '182.50.176.0'
 
 var location = ripe.ip2location(ip)
 console.log(location)
