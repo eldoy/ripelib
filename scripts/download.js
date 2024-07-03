ec313aff98883a780c5ef259cd13fec601f5ab86
acmb <48370302+acmb@users.noreply.github.com>
2024-07-03 18:46:56 +0100

    Download all RIR sources

--- scripts/download.js
+++ scripts/download.js
@@ -1,57 +1,51 @@
 var dugg = require('dugg')()
 var extras = require('extras')
 var fs = require('fs')
+var iso31661 = import('iso-3166').then(({ iso31661 }) => iso31661)
 
-function parseRipeNcc(filename) {
-  var data = fs.readFileSync(filename, 'utf8')
-
-  var rows = data.split('\n')
-  rows = rows.slice(4, rows.length)
-
-  return rows
-    .map((row, idx) => {
-      var [source, country, type, from, size, date, status] = row.split('|')
-      var to = rows[idx + 1]?.split('|')[3]
-
-      return { source, country, type, from, to, size, date, status }
-    })
-    .filter(({ source }) => !!source)
+function toCidr(value) {
+  return Math.round(-1 * Math.log2(value - 2) + 32)
 }
 
-function parseGeoLocations(filename) {
-  extras.get(`bzip2 -d ${filename}`)
+function parse(filename) {
+  var rir = filename.split('-')[1]
+  var data = fs.readFileSync(filename, 'utf8')
 
-  var data = fs.readFileSync(`${filename}.out`, 'utf8')
+  var rows = data
+    .split('\n')
+    .filter(
+      (v) =>
+        v.startsWith(rir) && !v.startsWith(rir + '|*|') && !v.includes('|asn|')
+    )
 
-  var rows = data.split('\n')
+  return rows
+    .map((row) => {
+      var [source, countryCode, type, from, size, date, status] = row.split('|')
 
-  return rows.map((row, idx) => {
-    var [
-      cidr,
-      id,
-      county,
-      city,
-      country,
-      country_code1,
-      country_code2,
-      ...coordinates
-    ] = row.split(',')
+      var cidr
 
-    var type
-    if (cidr.includes('.')) type = 'ipv4'
-    if (cidr.includes(':')) type = 'ipv6'
+      if (type == 'ipv4') {
+        cidr = from + '/' + toCidr(size)
+      } else if (type == 'ipv6') {
+        cidr = from + '/' + size
+      }
 
-    return {
-      cidr,
-      id,
-      county,
-      city,
-      country,
-      country_code1,
-      country_code2,
-      type
-    }
-  })
+      var { name: country } =
+        iso31661.find((iso) => iso.alpha2 == countryCode) || {}
+
+      return {
+        source,
+        countryCode,
+        country,
+        type,
+        cidr,
+        from,
+        size,
+        date,
+        status
+      }
+    })
+    .filter(({ source }) => !!source)
 }
 
 async function download({ url, parse }) {
@@ -64,18 +58,43 @@ async function download({ url, parse }) {
   fs.writeFileSync(`${filename}.json`, JSON.stringify(entries, null, 2))
 }
 
+var rirs = ['afrinic', 'apnic', 'arin', 'lacnic', 'ripencc']
+
 async function run() {
+  iso31661 = await iso31661
+
   extras.run(`rm -rf ./data/*`)
 
-  await download({
-    url: 'https://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-latest',
-    parse: parseRipeNcc
+  await Promise.all(
+    rirs.map((rir) => {
+      return download({
+        url: `https://ftp.ripe.net/pub/stats/${rir}/delegated-${rir}-extended-latest`,
+        parse
+      })
+    })
+  )
+
+  var ips = rirs.map((rir) => {
+    var data = require(`../data/delegated-${rir}-extended-latest.json`)
+    return data
   })
 
-  await download({
-    url: 'https://ftp.ripe.net/ripe/ipmap/geolocations-latest',
-    parse: parseGeoLocations
+  console.log({
+    ips: ips.map((rir) => ({
+      rir: rir[0]?.source,
+      asn: rir.filter(({ type }) => type == 'asn').length,
+      ipv4: rir.filter(({ type }) => type == 'ipv4').length,
+      ipv6: rir.filter(({ type }) => type == 'ipv6').length
+    }))
   })
+
+  ips = ips.flatMap((ip) => ip).sort((a, b) => a.from - b.from)
+  var ipv4 = ips.filter(({ type }) => type == 'ipv4')
+  var ipv6 = ips.filter(({ type }) => type == 'ipv6')
+
+  fs.writeFileSync(`./data/ipv4.json`, JSON.stringify(ipv4, null, 2))
+  fs.writeFileSync(`./data/ipv6.json`, JSON.stringify(ipv6, null, 2))
+  fs.writeFileSync(`./data/iso31661.json`, JSON.stringify(iso31661, null, 2))
 }
 
 run()
